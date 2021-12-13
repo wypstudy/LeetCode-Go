dd9e0fbb38bce02b3eb2b9190fc6f146cc7a45bb
Yiping Wei <yiping.wei@shopee.com>
2021-12-13 10:27:39 +0800

    807-城市天际线 便利2次

---
+++ P0000~P0999/807.go
@@ -0,0 +1,29 @@
+package P0000_P0999
+
+func maxIncreaseKeepingSkyline(grid [][]int) int {
+	var x, y [51]int
+	var ans = 0
+	for i, u := range grid {
+		for j, v := range u {
+			if v > x[i] {
+				x[i] = v
+			}
+			if v > y[j] {
+				y[j] = v
+			}
+		}
+	}
+	for i, u := range grid {
+		for j, v := range u {
+			ans += min(x[i]-v, y[j]-v)
+		}
+	}
+	return ans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
