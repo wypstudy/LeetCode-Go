59cbf5db5ef80a32134b82109f87bad02b53f42a
Yiping Wei <yiping.wei@shopee.com>
2022-04-07 11:27:09 +0800

    [796]RotateString

---
+++ P0700_P0799/796.go
@@ -0,0 +1,17 @@
+package main
+
+func rotateString(s string, goal string) bool {
+	n := len(s)
+	for i, _ := range s {
+		ns := s[n-i:n] + s[:n-i]
+		if ns == goal {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	println(rotateString("abcde", "cdeab"))
+	println(rotateString("abcde", "abced"))
+}
