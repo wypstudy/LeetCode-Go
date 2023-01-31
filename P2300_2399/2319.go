2a32b0ac0f726dbd68eb4576e33ba25625adbb65
Yiping Wei <yiping.wei@shopee.com>
2023-01-31 11:56:37 +0800

    solve: 2319

---
+++ P2300_2399/2319.go
@@ -0,0 +1,19 @@
+package main
+
+func checkXMatrix(grid [][]int) bool {
+	n := len(grid)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if i == j || n-j-1 == i {
+				if grid[i][j] == 0 {
+					return false
+				}
+			} else {
+				if grid[i][j] != 0 {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
